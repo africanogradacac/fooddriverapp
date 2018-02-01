80ff634cdf28632d5171b78ebd1363a791db0a2d
africanogradacac <35666225+africanogradacac@users.noreply.github.com>
2018-02-01 22:07:59 +0100

    Update config.js

--- www/js/config.js
+++ www/js/config.js
@@ -3,6 +3,6 @@
 var krms_driver_config ={	
 	'ApiUrl':"http://www.food.arizonaonline.org/driver/api",
 	'DialogDefaultTitle':"Food Driver App",	
-	'PushProjectID':"872019868979",	
+	'PushProjectID':"379436660204",	
 	'APIHasKey':"457845778422112545"
 };

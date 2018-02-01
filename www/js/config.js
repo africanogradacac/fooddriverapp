480932e0ac5bdcdd85d8481975a0d02c26668768
africanogradacac <damir_gradacac@yahoo.com>
2018-02-01 16:21:20 +0100

    tets

---
+++ www/js/config.js
@@ -0,0 +1,8 @@
+/*Driver App Configuration*/
+
+var krms_driver_config ={	
+	'ApiUrl':"http://www.food.arizonaonline.org/driver/api",
+	'DialogDefaultTitle':"Food Driver App",	
+	'PushProjectID':"872019868979",	
+	'APIHasKey':"457845778422112545"
+};
